573521ec58f98ec209292132e6b15107abfba603
uttkkarshh <utkarshtiwari.437@gmail.com>
2024-11-15 16:07:27 +0530

    first commit

---
+++ src/main/java/com/ut/kranti/KrantiApplication.java
@@ -0,0 +1,13 @@
+package com.ut.kranti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrantiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrantiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ut/kranti/KrantiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ut.kranti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrantiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
